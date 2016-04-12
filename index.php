0d9dc04ebd32db56f0aa827bb543fcb49456754f
guillaume23 <guillaume.batier@gmail.com>
2016-04-12 10:40:00 +0200

    add method addCustomer / deleteCustomer

--- index.php
+++ index.php
@@ -3,6 +3,16 @@ class Hello{
   public function __contruct(){
 
   }
+
+  public function addCustomer(){
+      // add customer
+  }
+
+  private function deleteCustomer(){
+      // delete customer
+  }
+
+
 }
 
 ?>
