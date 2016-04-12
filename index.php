c747b9ab131204de1e622c782eb90421325f462b
guillaume23 <guillaume.batier@gmail.com>
2016-04-12 09:53:07 +0200

    update index

--- index.php
+++ index.php
@@ -4,16 +4,16 @@ class Hello2{
   public function __contruct(){
 
   }
-}
+} 
 
 =======
 class Hello{
-	
+
 	public function __construct(){
-		
+
 	}
-	
-	
+
+
 }
 >>>>>>> origin/master
 echo "hello world";
